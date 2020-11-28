435e818834b14daf2340f1efbf2c22f2514c6674
paya02 <45tissue.pa8@gmail.com>
2020-11-28 12:33:07 +0900

    ユーザ認証のURIを独自のものに変更

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\Auth\Admin\ResetPasswordController;
+use App\Http\Controllers\Auth\LoginController as UserLogin;
+use App\Http\Controllers\Auth\ForgotPasswordController as UserForgotPassword;
+use App\Http\Controllers\Auth\ResetPasswordController as UserResetPassword;
+use App\Http\Controllers\Auth\Admin\ResetPasswordController as AdminResetPassword;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,13 +24,15 @@ Route::get('/', function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // ユーザー認証
-Auth::routes([
-    'register' => false,
-    'reset'    => true,
-    'verify'   => false
-]);
+Route::get('login', [UserLogin::class, 'showLoginForm'])->name('login');
+Route::post('login', [UserLogin::class, 'login']);
+Route::post('logout', [UserLogin::class, 'logout'])->name('logout');
+Route::get('password-reset', [UserForgotPassword::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password-email', [UserForgotPassword::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password-reset/new/{token}', [UserResetPassword::class, 'showResetForm'])->name('password.reset');
+Route::post('password-reset/new', [UserResetPassword::class, 'reset'])->name('password.update');
 
 // 管理者認証
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::get('password/reset/{token}', [AdminResetPassword::class, 'showResetForm'])->name('password.reset');
 });
