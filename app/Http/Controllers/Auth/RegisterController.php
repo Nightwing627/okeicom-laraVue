52b0c5ce280458b9a328d711cb9d6b5e2f7072f6
paya02 <45tissue.pa8@gmail.com>
2020-12-03 09:43:56 +0900

    パスワードの最低桁数を8桁に揃える

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -164,7 +164,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
             'account' => ['required', 'regex:/^[a-zA-Z0-9]+$/', 'max:255'],
             'sex' => ['required', 'numeric'],
             'profile' => ['required', 'string'],
