6b982593c887ce52c43a9458d40f33926d46372a
paya02 <45tissue.pa8@gmail.com>
2020-12-22 11:38:54 +0900

    TOPページ表示の機能を実装

--- config/const.php
+++ config/const.php
@@ -10,6 +10,8 @@ return [
         // 受講者登録済みレッスン一覧
         'attendanceLesson' => 10,
     ],
+    // TOPページサムネイル表示件数
+    'top_thumbnail_count' => 5,
     // 画像保存フォルダ名(storage/appからの相対パス)
     'image_path' => [
         // コース
