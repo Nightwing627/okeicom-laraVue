b909cea5e2d9c8c75ec319f898578cbb52483c35
nakazawakan <treby@example.netgit config --global user.email chankan77@gmail.com>
2021-06-30 23:11:50 +0900

    微調整

--- resources/views/common/sidebar-user.blade.php
+++ resources/views/common/sidebar-user.blade.php
@@ -9,8 +9,8 @@
 		<li class="@if(Request::is('mypage/u/profile')) selected @endif"><a href="{{ route('mypage.u.profile') }}">プロフィール</a></li>
 		{{-- <li class="@if(Request::is('mypage/u/message')) selected @endif"><a href="{{ route('mypage.u.') }}">支払い管理</a></li> --}}
 		{{-- <li class="@if(Request::is('mypage/u/trade')) selected @endif"><a href="{{ route('mypage.u.creditcards') }}">クレジットカード情報</a></li> --}}
-		{{--  <li class="@if(Request::is('mypage/u/bank')) selected @endif"><a href="{{ route('mypage.u.bank.show') }}">銀行口座情報</a></li>  --}}
-		{{--  <li class="@if(Request::is('mypage/u/trade')) selected @endif"><a href="{{ route('mypage.u.trade') }}">入出金管理</a></li>  --}}
-		{{--  <li class="@if(Request::is('mypage/u/withdrawal')) selected @endif"><a href="{{ route('mypage.u.withdrawal.create') }}">退会</a></li>  --}}
+		{{-- <li class="@if(Request::is('mypage/u/bank')) selected @endif"><a href="{{ route('mypage.u.bank.show') }}">銀行口座情報</a></li>  --}}
+		{{-- <li class="@if(Request::is('mypage/u/trade')) selected @endif"><a href="{{ route('mypage.u.trade') }}">入出金管理</a></li>  --}}
+		{{-- <li class="@if(Request::is('mypage/u/withdrawal')) selected @endif"><a href="{{ route('mypage.u.withdrawal.create') }}">退会</a></li>  --}}
 	</ul>
 </div>
